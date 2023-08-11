405f1995bbf8bccd887320af83a0bbf028d73832
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-08-10 22:24:48 -0300

    Accuracy score added to output table

--- the_all_mite.py
+++ the_all_mite.py
@@ -49,9 +49,12 @@ print(">>> Step 01 done!\n")
 
 # create dataframe from model 1 predictions
 print("### Step 02: Starting first prediction: MITE/not MITE")
-results_1 = np.argmax(MODEL_1.predict(sequence_arrays), axis=1)
+r1 = MODEL_1.predict(sequence_arrays)
+results_1 = np.argmax(r1, axis=1)
+accuracy_1 = np.max(r1, axis=1)
 df1 = pd.DataFrame(test_records)
 df1['prediction_1'] = results_1
+df1['accuracy_1'] = accuracy_1
 df1['prediction_1'] = df1['prediction_1'].map({0: 'NM', 1: 'MITE'})
 print(">>> Step 02 done!\n")
 
@@ -59,10 +62,12 @@ print(">>> Step 02 done!\n")
 # select sequences classified as not MITE
 not_mite_pred_1 = df1.loc[df1['prediction_1'] != 'MITE']
 drop_not_mite = df1.loc[df1['prediction_1'] != 'MITE'].index.to_list()  # get index to drop from sequence array
-not_mite_pred_1 = not_mite_pred_1.loc[:, ['id', 'len', 'prediction_1']]
+not_mite_pred_1 = not_mite_pred_1.loc[:, ['id', 'len', 'prediction_1', 'accuracy_1']]
 
 # Concatenate sequences filtered as not MITE by length and the ones classified as not MITE
-not_mite = pd.concat([not_mite_filtered, not_mite_pred_1])
+not_mite = pd.concat([not_mite_filtered, not_mite_pred_1]).fillna(1)
+not_mite['prediction_2'] = not_mite['prediction_1']
+not_mite['accuracy_2'] = not_mite['accuracy_1']
 
 # drop sequences not classified as MITE by model 1
 sequence_arrays = np.delete(sequence_arrays, drop_not_mite, axis=0)
@@ -72,12 +77,15 @@ mites = df1.loc[df1['prediction_1'] == 'MITE']
 
 # Predict Stowaway and Tourist
 print("### Step 03: Starting second prediction: Stowaway/Tourist")
-results_2 = np.argmax(MODEL_2.predict(sequence_arrays), axis=1)
+r2 = MODEL_2.predict(sequence_arrays)
+results_2 = np.argmax(r2, axis=1)
+accuracy_2 = np.max(r2, axis=1)
 mites['prediction_2'] = results_2
+mites['accuracy_2'] = accuracy_2
 mites['prediction_2'] = mites['prediction_2'].map({0: 'Stowaway', 1: 'Tourist'})
-mites = mites.loc[:, ['id', 'len', 'prediction_1', 'prediction_2']]
+mites = mites.loc[:, ['id', 'len', 'prediction_1', 'accuracy_1', 'prediction_2', 'accuracy_2']]
 print(">>> Step 03 done!\n")
 
 # Create final table
-final_results = pd.concat([mites, not_mite]).fillna('NM')
+final_results = pd.concat([mites, not_mite]).round(4)
 final_results.to_csv(OUT_TABLE, sep='\t', index=False)
